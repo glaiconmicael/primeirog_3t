731af2a82e09398b7a8f6ddb9e848a25a9d5c8c5
glaiconmicael <110247559+glaiconmicael@users.noreply.github.com>
2022-10-07 12:49:57 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,13 @@
 
+function total(){
+    let c = document.getElementById("capital").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let m = 0;
+   for (let i = 1; m <= t; i ++){
+    m = c * (1 + (j/100));
+    document.write("valor do mes " + i + " = " + m + "<br>");
+    c = m;
+   }
+ document.write("montante: " + m)
+}
